346c708561d057e5ffbf3a6dab087ae4bd3ff3b3
Ilya Pridanov <pridanov.ilya@mail.ru>
2020-04-09 13:33:09 +0400

    добавляет контент в блок новое

--- source/js/modules/getSlider.js
+++ source/js/modules/getSlider.js
@@ -1,51 +1,53 @@
 'use strict';
 
 (function () {
-    var sliderContainers = document.querySelectorAll('.slider');
+  var sliderContainers = document.querySelectorAll('.slider');
 
-    var getSlider = function (slider) {
-        if (slider) {
-            var container = slider.querySelector('.swiper-container');
-            var next = slider.querySelector('.js-btn-next');
-            var prev = slider.querySelector('.js-btn-prev');
-            var pagination = slider.querySelector('.swiper-pagination');
-            var slidesPerView = 1;
-            var spaceBetween = 20;
-            return new window.Swiper(container, {
-                loop: true,
-                navigation: {
-                nextEl: next,
-                prevEl: prev,
-                },
-                pagination: {
-                el: pagination,
-                clickable: true,
-                },
-                slidesPerView: slidesPerView,
-                spaceBetween: spaceBetween,
-                // breakpoints: {
-                //   320: {
-                //     slidesPerView: 1,
-                //     spaceBetween: spaceBetween,
-                //   },
-                //   600: {
-                //     slidesPerView: slidesPerView600,
-                //     spaceBetween: spaceBetween,
-                //   },
-                //   1000: {
-                //     slidesPerView: slidesPerView,
-                //     spaceBetween: spaceBetween,
-                //   },
-                // },
-            });
-        }
-    };
+  var getPhotoSlider = function (slider) {
+    if (slider) {
+      var container = slider.querySelector('.swiper-container');
+      var next = slider.querySelector('.js-btn-next');
+      var prev = slider.querySelector('.js-btn-prev');
+      var pagination = slider.querySelector('.swiper-pagination');
+      var slidesPerView = 1;
+      var slidesPerView600 = 1;
+      var spaceBetween = 0;
+      if (slider.classList.contains('hit-new')) {
+        slidesPerView = 4;
+        slidesPerView600 = 2;
+        spaceBetween = 30;
+      }
+      return new window.Swiper(container, {
+        loop: true,
+        navigation: {
+          nextEl: next,
+          prevEl: prev,
+        },
+        pagination: {
+          el: pagination,
+          clickable: true,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: spaceBetween,
+          },
+          600: {
+            slidesPerView: slidesPerView600,
+            spaceBetween: spaceBetween,
+          },
+          1000: {
+            slidesPerView: slidesPerView,
+            spaceBetween: spaceBetween,
+          },
+        },
+      });
+    }
+  };
 
-    window.onload = function () {
-        sliderContainers.forEach(function (currentValue) {
-            getSlider(currentValue);
-        });
-    };
+  sliderContainers.forEach(function (currentValue) {
+    getPhotoSlider(currentValue);
+  });
 
-    window.getSlider = getSlider;
+
 })();
--- source/js/script.js
+++ source/js/script.js
@@ -3,14 +3,7 @@
 // Vendor
 //= vendor/swiper.js
 //= vendor/modernizr-custom.js
-//= vendor/imask.js
-//= vendor/jquery-3.4.1.min.js
-//= vendor/jquery.fancybox.js
+
 
 // Modules
-//= modules/main.js
-//= modules/nav.js
 //= modules/getSlider.js
-//= modules/formMask.js
-//= modules/fancy-card.js
-//= modules/gallery-mobile.js
